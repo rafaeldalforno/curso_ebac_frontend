bdebfdea39cd60124cb8cfaee7154c4e14179c62
Rafael Dal Forno <rafa_fln15@hotmail.com>
2023-10-09 15:59:01 -0300

    primeiro commit da branch exercicio_jquery

--- main.js
+++ main.js
@@ -1,42 +1,19 @@
-const form = document.getElementById('form-number');
-let formEhValido = false;
-const numeroA = document.getElementById('numberA');
-const numeroB = document.getElementById('numberB');
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault();
 
-function validaNumber(numeroA, numeroB){
-    console.log('A:' + numeroA);
-    console.log('B:' + numeroB);
-    if (numeroA < numeroB){
-        formEhValido = true;
-    } else{
-        formEhValido = false;
-    }
-    return formEhValido;
-}
+        const novaTarefa = $('#tarefa-cadastro').val();
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
+        const novaLinha = $('<li></li>');
 
-    const mensagemSucesso = `O número B= ${numeroB.value} é maior que o numero A= ${numeroA.value}. Parabéns!`;
-    const mensagemError = `O número A= ${numeroA.value} é maior que o número B= ${numeroB.value}. Tente Novamente!`;
+        $(`<a>${novaTarefa}</a>`).appendTo(novaLinha);
+        $(novaLinha).appendTo('ul');
+        $('#tarefa-cadastro').val('');
 
-    formEhValido = validaNumber(numeroA.value, numeroB.value);
-    
 
-    if (formEhValido) {
-        const containerMensagemSucesso = document.querySelector('.success-message');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block';
-        numeroA.classList.remove('error');
-        document.querySelector('.error-message').style.display = 'none';
-
-        numeroA.value = '';
-        numeroB.value = '';
-    } else {
-        const containerMensagemError = document.querySelector('.error-message');
-        containerMensagemError.innerHTML = mensagemError;
-        numeroA.classList.add('error');
-        document.querySelector('.error-message').style.display = 'block';
-        document.querySelector('.success-message').style.display = 'none';
-    }
+        // Adicionar uma classe ao item que for clicado
+        $('a').click(function(){
+            $(this).addClass('item-clicado');
+        })
+    })
 })
